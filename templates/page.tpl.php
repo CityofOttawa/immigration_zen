04f2f0855bfe9eff2cbb4ccc89e4337dbae7bf4c
vagrant <vagrant@localhost.localdomain>
2013-07-27 19:26:57 +0000

    Fixed navigation styling and dissapearing Added styling for search Added footer styling Added footer navigation styling Fixed line-height for headings

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -102,13 +102,39 @@
 		<form id="search" action="#" method="post">
 
 				<label for="search">Search City of Ottawa</label>
-				<input id="search" type="search" name="site-search"><br>
+				<input id="search" type="search" name="site-search" placeholder="Search...">
 
 				<button type="submit" class="search-btn">Search</button>
 		</form>
 	</div><!-- /header-box -->
-
 	</header>
+  <div id="navigation">
+
+    <?php if ($main_menu): ?>
+      <nav id="main-menu" role="navigation">
+        <?php
+        // This code snippet is hard to modify. We recommend turning off the
+        // "Main menu" on your sub-theme's settings form, deleting this PHP
+        // code block, and, instead, using the "Menu block" module.
+        // @see http://drupal.org/project/menu_block
+        print theme('links__system_main_menu', array(
+          'links' => $main_menu,
+          'attributes' => array(
+            'class' => array('links', 'inline', 'clearfix'),
+          ),
+          'heading' => array(
+            'text' => t('Main menu'),
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
+      </nav>
+    <?php endif; ?>
+
+    <?php print render($page['navigation']); ?>
+
+  </div><!-- /#navigation -->
+
   <div id="main">
 
     <div id="content" class="column" role="main">
@@ -130,32 +156,7 @@
       <?php print $feed_icons; ?>
     </div><!-- /#content -->
 
-    <div id="navigation">
 
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see http://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-      <?php print render($page['navigation']); ?>
-
-    </div><!-- /#navigation -->
 
     <?php
       // Render the sidebars to see if there's anything in them.
